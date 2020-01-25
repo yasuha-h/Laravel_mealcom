0b400883a3dc87041ee573d940d0ee1fe53afa74
yasuha-h <kurukami135@gmail.com>
2020-01-25 16:00:12 +0900

    test_blade_hello-world

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('hello world')
+  <p>Hello World</p>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mealcom</title>
+    <link rel="stylesheet" type="text/css" href="./sass/style.css">
+  </head>
+  <body>
+    @yield('hello world')
+  </body>
+</html>
