22bbbb7a22f32e23faeb4f416aed1cb25eb5a1cf
yasuha-h <kurukami135@gmail.com>
2020-01-27 23:35:30 +0900

    bootstrap_fixpoint

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,9 +11,9 @@
     <header>
     <div class="container">
       <div class="row">
-        <div class="title-logo col-xs-4">Mealcom</div>
-        <div class="col-xs-8">
-          <ul>
+        <div class="col-6">Mealcom</div>
+        <div class="col-6">
+          <ul class="list-unstyled row">
             <li>
               <a href="#">サインアップ</a>
             </li>
