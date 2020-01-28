7ee6fe4380e53f7e7de3a394b6eb9ba81fddb450
yasuha-h <kurukami135@gmail.com>
2020-01-28 21:33:00 +0900

    create_header_nosignin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,16 +9,16 @@
   </head>
   <body>
     <header>
-    <div class="container">
+    <div class="container py-5 border-bottom">
       <div class="row">
-        <div class="col-6">Mealcom</div>
-        <div class="col-6">
-          <ul>
-            <li>
-              <a href="#">サインアップ</a>
+        <h2 class="col-3 text-center align-self-center">Mealcom</h2>
+        <div class="col-5 offset-4 d-flex">
+          <ul class="list-unstyled row w-100">
+            <li class="align-self-center w-50">
+              <a href="#" class="d-block text-center">サインアップ</a>
             </li>
-            <li>
-              <a href="#">サインイン</a>
+            <li class="align-self-center w-50">
+              <a href="#" class="d-block text-center">サインイン</a>
             </li>
           </ul>
         </div>
