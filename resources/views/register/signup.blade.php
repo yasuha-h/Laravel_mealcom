2dec364e090d8f575b27cedd71f8d5309e363970
yasuha-h <kurukami135@gmail.com>
2020-01-30 19:47:23 +0900

    fix_a-bit_input-form

--- resources/views/register/signup.blade.php
+++ resources/views/register/signup.blade.php
@@ -3,32 +3,32 @@
 <!-- <label class="col-sm-2 control-label" for="username">名前：</label> -->
 <div class="container">
   <div class="row">
-    <div class="col-10 offset-1 col-sm-6 offset-sm-3 pt-5 text-center">
+    <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
       <input type="text" class="w-75 w-sm-50 rounded" id="username" name="username" 
       placeholder="メールアドレスを入力してください" value="{{ old('username') }}">
     </div>
   </div>
   <div class="row">
-    <div class="col-6 offset-3 pt-5 text-center">
-      <input type="text" class="w-50 rounded" id="username" name="username" 
+    <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
+      <input type="text" class="w-75 w-sm-50 rounded" id="username" name="username" 
       placeholder="ユーザー名を入力してください" value="{{ old('username') }}">
     </div>
   </div>
   <div class="row">
-    <div class="col-6 offset-3 pt-5 text-center">
-      <input type="text" class="w-50 rounded" id="username" name="username" 
+    <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
+      <input type="text" class="w-75 w-sm-50 rounded" id="username" name="username" 
       placeholder="パスワード" value="{{ old('username') }}">
     </div>
   </div>
   <div class="row">
-    <div class="col-6 offset-3 pt-5 text-center">
-      <input type="text" class="w-50 rounded" id="username" name="username" 
+    <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
+      <input type="text" class="w-75 w-sm-50 rounded" id="username" name="username" 
       placeholder="パスワード(再確認)" value="{{ old('username') }}">
     </div>
   </div>
   <div class="row">
-    <div class="col-6 offset-3 pt-5 text-center">
-      <input type="text" class="w-50 rounded" id="username" name="username" 
+    <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
+      <input type="text" class="w-75 w-sm-50 rounded" id="username" name="username" 
       placeholder="性別" value="{{ old('username') }}">
     </div>
   </div>
