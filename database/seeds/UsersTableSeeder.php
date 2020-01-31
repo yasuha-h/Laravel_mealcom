9c2b25d9ec41336f2f77c07a99547f417fbdfc3f
yasuha-h <kurukami135@gmail.com>
2020-02-01 00:00:11 +0900

    create_UsersTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,17 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+      DB::table('users')->insert([
+        'name' => Str::random(10),
+        'email' => Str::random(10).'@gmail.com', 
+        'mealcom_id' => Str::random(10),
+        'password' => bcrypt('secret'),
+        'sex_code' => 2,
+        'profile' => Str::random(140),
+        'thumbnail' => Str::random(10),
+        'header_img' => Str::random(10),
+        'created_at' => now(),
+        'updated_at' => now(),
+      ]);
     }
 }
