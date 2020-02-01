01b9c765e805da3436b203b8209ddf2d926a0b70
yasuha-h <kurukami135@gmail.com>
2020-02-01 17:31:23 +0900

    test_seed_with_dummy

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -11,6 +11,21 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+      DB::table('posts')->insert(
+        [
+        'user_id' => 1,
+        'img_1' => Str::random(10),
+        'content' => Str::random(10),
+        'created_at' => now(),
+        'updated_at' => now()
+        ],
+        [
+          'user_id' => 1,
+          'img_1' => Str::random(10),
+          'content' => Str::random(10),
+          'created_at' => now(),
+          'updated_at' => now()
+        ]
+    );
     }
 }
