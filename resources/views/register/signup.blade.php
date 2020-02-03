7bf8c55319951688284d014e2a766a8a897e12cf
yasuha-h <kurukami135@gmail.com>
2020-02-03 22:39:29 +0900

    create_layout_common.errors

--- resources/views/register/signup.blade.php
+++ resources/views/register/signup.blade.php
@@ -1,40 +1,44 @@
 @extends('layouts.app')
 @section('signup-form')
-<!-- <label class="col-sm-2 control-label" for="username">名前：</label> -->
-<div class="container">
-  <div class="row">
-    <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
-      <input type="text" class="w-75 w-sm-50 rounded" id="username" name="username" 
-      placeholder="メールアドレスを入力してください" value="{{ old('username') }}">
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
-      <input type="text" class="w-75 w-sm-50 rounded" id="username" name="username" 
-      placeholder="ユーザー名を入力してください" value="{{ old('username') }}">
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
-      <input type="text" class="w-75 w-sm-50 rounded" id="username" name="username" 
-      placeholder="パスワード" value="{{ old('username') }}">
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
-      <input type="text" class="w-75 w-sm-50 rounded" id="username" name="username" 
-      placeholder="パスワード(再確認)" value="{{ old('username') }}">
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
-      <input type="text" class="w-75 w-sm-50 rounded" id="username" name="username" 
-      placeholder="性別" value="{{ old('username') }}">
-    </div>
-  </div>
-</div>
-@endsection
 
-@section('signup-css')
-<link rel="stylesheet" type="text/css" href="{{ asset('css/signup/signup.css') }}">
-@endsection
+@include('common.errors')
+<div class="container">
+  <form action="" method="POST" >
+
+    <div class="row">
+      <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
+        <input type="text" class="w-75 w-sm-50 rounded" id="username" name="username" 
+        placeholder="メールアドレスを入力してください" value="{{ old('username') }}">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
+        <input type="text" class="w-75 w-sm-50 rounded" id="username" name="username" 
+        placeholder="ユーザー名を入力してください" value="{{ old('username') }}">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
+        <input type="text" class="w-75 w-sm-50 rounded" id="username" name="username" 
+        placeholder="パスワード" value="{{ old('username') }}">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
+        <input type="text" class="w-75 w-sm-50 rounded" id="username" name="username" 
+        placeholder="パスワード(再確認)" value="{{ old('username') }}">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
+        <input type="text" class="w-75 w-sm-50 rounded" id="username" name="username" 
+        placeholder="性別" value="{{ old('username') }}">
+      </div>
+    </div>
+  </form>
+  </div>
+  @endsection
+  
+  @section('signup-css')
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/signup/signup.css') }}">
+  @endsection
