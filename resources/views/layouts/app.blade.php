e99811941c194f0c952bbeb2f52dec58c7567178
yasuha-h <kurukami135@gmail.com>
2020-02-05 22:18:28 +0900

    add_meta_csrf-token

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Mealcom</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app_ex.css') }}">
