15c858d14a4decc1dc8c53f6291eced0089d9031
yasuha-h <kurukami135@gmail.com>
2020-02-06 20:00:29 +0900

    add_mealcom-id_to_signup-form,fix_Model-User_&_Controller-Register_AND_delete_UserTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,17 +12,17 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
       DB::table('users')->truncate();
-      DB::table('users')->insert([
-        'name' => Str::random(10),
-        'email' => Str::random(10).'@gmail.com', 
-        'mealcom_id' => Str::random(10),
-        'password' => bcrypt('secret'),
-        'sex_code' => 2,
-        'profile' => Str::random(140),
-        'thumbnail' => Str::random(10),
-        'header_img' => Str::random(10),
-        'created_at' => now(),
-        'updated_at' => now(),
-      ]);
+      // DB::table('users')->insert([
+      //   'name' => Str::random(10),
+      //   'email' => Str::random(10).'@gmail.com', 
+      //   'mealcom_id' => Str::random(10),
+      //   'password' => Hash::make('test_password'),
+      //   'sex_code' => 2,
+      //   'profile' => Str::random(10),
+      //   'thumbnail' => Str::random(10),
+      //   'header_img' => Str::random(10),
+      //   'created_at' => now(),
+      //   'updated_at' => now(),
+      // ]);
     }
 }
--- resources/views/register/signup.blade.php
+++ resources/views/register/signup.blade.php
@@ -7,20 +7,26 @@
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <div class="row">
       <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
-        <input type="email" class="form-control" id="email" name="email" 
-        placeholder="メールアドレスを入力してください" value="{{ old('email') }}">
+        <input type="text" class="form-control" id="user_name" name="name" 
+        placeholder="ユーザー名" value="{{ old('name') }}">
       </div>
     </div>
     <div class="row">
       <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
-        <input type="text" class="form-control" id="user_name" name="name" 
-        placeholder="ユーザー名を入力してください" value="{{ old('name') }}">
+        <input type="text" class="form-control" id="mealcom_id" name="mealcom_id" 
+        placeholder="mealcom ID" value="{{ old('mealcom_id') }}">
       </div>
     </div>
     <div class="row">
       <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
         <input type="password" class="form-control" id="password" name="password" 
         placeholder="パスワード" value="{{ old('password') }}">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-10 offset-1 col-md-6 offset-sm-3 pt-5 text-center">
+        <input type="email" class="form-control" id="email" name="email" 
+        placeholder="メールアドレス" value="{{ old('email') }}">
       </div>
     </div>
     <div class="row">
