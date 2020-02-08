6716a8de870aa5ee1bb8e095c1d4ce4fdfbede14
yasuha-h <kurukami135@gmail.com>
2020-02-08 21:40:47 +0900

    create_common-head.blade_&_components-header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,24 @@
+@auth
+<header>
+  <div class="container py-3 border-bottom">
+    <div class="row">
+      <div class="col-8 offset-2 d-flex">
+        <ul class="list-unstyled row w-100">
+          <li class="align-self-center w-25">
+            <a href="/signup" class="d-block text-center">シェア</a>
+          </li>
+          <li class="align-self-center w-25">
+            <a href="#" class="d-block text-center">フォロー</a>
+          </li>
+          <li class="align-self-center w-25">
+            <a href="#" class="d-block text-center">フォロワー</a>
+          </li>
+          <li class="align-self-center w-25">
+            <a href="#" class="d-block text-center">ナイス</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</header>
+@endauth
