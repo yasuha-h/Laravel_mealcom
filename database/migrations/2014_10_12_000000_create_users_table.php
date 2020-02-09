18494008ad72841d404685911072a6722d6f0078
yasuha-h <kurukami135@gmail.com>
2020-02-09 23:12:20 +0900

    organize_users-migration_&_add_users-seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('mealcom_id')->unique()->nullable(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('sex_code')->length(1)->nullable()->comment('性別コード');
+            $table->text('profile',140)->nullable();
+            $table->binary('thumbnail')->nullable();
+            $table->binary('header_img')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
