eb727896f5cec74d8312e3ac54bc47619c5196d5
yasuha-h <kurukami135@gmail.com>
2020-02-10 23:34:56 +0900

    add_profile.blade-template

---
+++ resources/views/user/profile.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@include('components.header')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <!-- ユーザープロフィール  -->
+      <div class="col-6"></div>
+      <!-- ユーザーシェア -->
+      <div class="col-6"></div>
+    </div>
+  </div>
+@endsection
