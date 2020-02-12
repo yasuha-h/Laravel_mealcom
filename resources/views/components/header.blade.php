28681eb9ccb697289d4016372b88be7acc70102d
yasuha-h <kurukami135@gmail.com>
2020-02-12 21:36:34 +0900

    change_header-layout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,24 +1,38 @@
-@auth
 <header>
-  <div class="container py-3 border-bottom">
+  <div class="container py-1 border-bottom">
     <div class="row">
-      <div class="col-8 offset-2 d-flex">
-        <ul class="list-unstyled row w-100">
-          <li class="align-self-center w-25">
-            <a href="/user/{id}/profile" class="d-block text-center">シェア</a>
-          </li>
-          <li class="align-self-center w-25">
-            <a href="/user/{id}/following" class="d-block text-center">フォロー</a>
-          </li>
-          <li class="align-self-center w-25">
-            <a href="#" class="d-block text-center">フォロワー</a>
-          </li>
-          <li class="align-self-center w-25">
-            <a href="#" class="d-block text-center">ナイス</a>
-          </li>
-        </ul>
+      <h2 class="col-3 text-center align-self-center">Mealcom</h2>
+      <div class="col-7 offset-2 d-flex">
+        @auth
+          <ul class="list-unstyled row w-100">
+            <li class="align-self-center w-25 offset-2">
+              <a href="/user/{id}" class="d-block text-center">プロフィール</a>
+            </li>
+            <li class="align-self-center w-25">
+              <a href="#" class="d-block text-center"
+                onclick="document.getElementById('logout-form').submit();">
+                ログアウト
+              </a>
+              <form id="logout-form" action="{{'/logout'}}" method="POST" style="display: none;">
+                @csrf
+              </form>
+            </li>
+            <li class="align-self-center w-25">
+              <a href="/share" class="d-block text-center">シェア</a>
+            </li>
+          </ul>
+        @endauth
+        @guest
+          <ul class="list-unstyled row w-100">
+            <li class="align-self-center w-50">
+              <a href="/signup" class="d-block text-center">サインアップ</a>
+            </li>
+            <li class="align-self-center w-50">
+              <a href="/login" class="d-block text-center">サインイン</a>
+            </li>
+          </ul>
+        @endguest
       </div>
     </div>
   </div>
 </header>
-@endauth
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,44 +10,6 @@
     @yield('css')
   </head>
   <body>
-    <header>
-    <div class="container py-5 border-bottom">
-      <div class="row">
-        <h2 class="col-3 text-center align-self-center">Mealcom</h2>
-        <div class="col-5 offset-4 d-flex">
-          @auth
-          <ul class="list-unstyled row w-100">
-              <li class="align-self-center w-25">
-                <a href="/user/{id}" class="d-block text-center"></a>
-              </li>
-              <li class="align-self-center w-25">
-                <a href="#" class="d-block text-center"
-                  onclick="document.getElementById('logout-form').submit();">
-                  ログアウト
-                </a>
-                <form id="logout-form" action="{{'/logout'}}" method="POST" style="display: none;">
-                  @csrf
-                </form>
-              </li>
-              <li class="align-self-center w-25">
-                <a href="/share" class="d-block text-center">シェア</a>
-              </li>
-            </ul>
-          @endauth
-          @guest
-            <ul class="list-unstyled row w-100">
-              <li class="align-self-center w-50">
-                <a href="/signup" class="d-block text-center">サインアップ</a>
-              </li>
-              <li class="align-self-center w-50">
-                <a href="/login" class="d-block text-center">サインイン</a>
-              </li>
-            </ul>
-          @endguest
-        </div>
-      </div>
-    </div>
-    </header>
     @include('components.header')
     @yield('content')
   </body>
