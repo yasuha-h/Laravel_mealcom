8f9221f176ea9c21faa890a84914b685c78acf93
yasuha-h <kurukami135@gmail.com>
2020-02-14 00:05:08 +0900

    create_navbar.blade.php_&_add_navbar_to_profile.blade

---
+++ resources/views/common/navbar.blade.php
@@ -0,0 +1,6 @@
+<nav class="nav nav-tabs col-1 text-center pr-0">
+    <a href="#" class="nav-item nav-link w-100 p-0 align-middle">シェア</a>
+    <a href="#" class="nav-item nav-link w-100 p-0 align-middle">フォロー</a>
+    <a href="#" class="nav-item nav-link w-100 p-0">フォロワー</a>
+    <a href="#" class="nav-item nav-link w-100 p-0">いいね</a>
+</nav>
--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -3,14 +3,16 @@
   <div class="container">
     <div class="row">
       <!-- ユーザープロフィール  -->
-      <div class="col-4 offset-1 row pt-5 border-right">
+      <div class="col-4 row pt-5 px-0 mx-0 text-center">
         <h3 class="col-10 text-center pt-5">{{ $user->name }}</h3>
         <div class="col-10 text-center">ID:{{ $user->mealcom_id }}</div>
         <div class="col-10 text-center pt-5">性別:{{ $user->sex_code }}</div>
         <div class="col-10 text-center pt-5">{{ $user->profile }}</div>
       </div>
+      @include('common.navbar')
       <!-- ユーザーシェア -->
-      <div class="col-6"></div>
+      <div class="col-7 border-left px-0">
+      </div>
     </div>
   </div>
 @endsection
