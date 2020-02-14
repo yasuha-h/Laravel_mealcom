34aecddca719fe6a1350136cdbe55c4162f85690
yasuha-h <kurukami135@gmail.com>
2020-02-14 22:28:14 +0900

    fix_header_layout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,14 +2,14 @@
   <div class="container py-1 border-bottom">
     <div class="row">
       <h2 class="col-3 text-center align-self-center">Mealcom</h2>
-      <div class="col-7 offset-2 d-flex">
+      <div class="col-6 offset-3 d-flex">
         @auth
           <ul class="list-unstyled row w-100">
-            <li class="align-self-center w-25 offset-2">
-              <a href="user/{{ $user->id }}" class="d-block text-center">プロフィール</a>
+            <li class="align-self-center col-3 offset-3">
+              <a href="user/{{ $user->id }}" class="p-1 d-block text-center">プロフィール</a>
             </li>
-            <li class="align-self-center w-25">
-              <a href="#" class="d-block text-center"
+            <li class="align-self-center col-3">
+              <a href="#" class="d-block text-center p-1"
                 onclick="document.getElementById('logout-form').submit();">
                 ログアウト
               </a>
@@ -17,17 +17,17 @@
                 @csrf
               </form>
             </li>
-            <li class="align-self-center w-25">
-              <a href="/share" class="d-block text-center">シェア</a>
+            <li class="align-self-center col-3">
+              <a href="/share" class="d-block text-center p-1">シェア</a>
             </li>
           </ul>
         @endauth
         @guest
           <ul class="list-unstyled row w-100">
-            <li class="align-self-center w-50">
+            <li class="align-self-center col-3 offset-6">
               <a href="/signup" class="d-block text-center">サインアップ</a>
             </li>
-            <li class="align-self-center w-50">
+            <li class="align-self-center col-3">
               <a href="/login" class="d-block text-center">サインイン</a>
             </li>
           </ul>
