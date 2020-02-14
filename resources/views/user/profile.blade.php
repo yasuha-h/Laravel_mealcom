fc065686e7f75bf1878321aa04200074b62e0bf8
yasuha-h <kurukami135@gmail.com>
2020-02-14 22:14:21 +0900

    fix_profile_spacing

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -3,11 +3,11 @@
   <div class="container">
     <div class="row">
       <!-- ユーザープロフィール  -->
-      <div class="col-4 row pt-5 px-0 mx-0 text-center">
-        <h3 class="col-10 text-center pt-5">{{ $user->name }}</h3>
-        <div class="col-10 text-center">ID:{{ $user->mealcom_id }}</div>
-        <div class="col-10 text-center pt-5">性別:{{ $user->sex_code }}</div>
-        <div class="col-10 text-center pt-5">{{ $user->profile }}</div>
+      <div class="col-4 row px-0 mx-0 mb-5 py-5 text-center">
+        <h3 class="col-12 text-center m-0">{{ $user->name }}</h3>
+        <div class="col-12 text-center">ID:{{ $user->mealcom_id }}</div>
+        <div class="col-12 text-center">性別:{{ $user->sex_code }}</div>
+        <div class="col-12 text-center mb-5">{{ $user->profile }}</div>
       </div>
       @include('common.navbar')
       <!-- ユーザーシェア -->
