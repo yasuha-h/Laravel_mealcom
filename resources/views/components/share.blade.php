7d1a33793a6465c801ca9e51c9bac56d0ae8e8dd
yasuha-h <kurukami135@gmail.com>
2020-02-23 00:35:44 +0900

    trim_nices_layout

--- resources/views/components/share.blade.php
+++ resources/views/components/share.blade.php
@@ -1,15 +1,16 @@
-<div class="container rounded row">
-  @foreach ($posts as $post)
+@foreach ($posts as $post)
+  <div class="container-40 border-grey row pt-3 pb-0 px-0 rounded-lg">
     <div class="offset-1 col-10 text-center">
       <!-- text-content -->
       {{ $post->content }}
     </div>
-  @endforeach
-  <div class="offset-1 col-10 text-center">
-    <!-- img -->
-    <!-- {{ $user }} -->
+    <div class="offset-1 col-10 text-center">
+      <!-- img -->
+      {{ $post->img_1 }}
+    </div>
+    <div class="offset-1 col-10 text-center"></div>
+    <div class="col-1 p-0">
+      <i class="fas fa-heart"></i>
+    </div>
   </div>
-  <div class="col-1">
-    <i class="fas fa-heart"></i>
-  </div>
-</div>
+@endforeach
