155d9935d625ff80014cb21cdf48d25e506bcc2f
yasuha-h <kurukami135@gmail.com>
2020-03-06 23:31:35 +0900

    can_show_post_of_following-user_&_self

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Post;
+use App\Follower;
 
 class HomeController extends Controller
 {
     public function index(){
         $user = Auth::user();
-        $posts = Post::where('user_id', $user->id)
-                        ->select('img_1', 'content')
-                        ->get();
+        $posts = Post::whereIn('user_id', 
+                    Follower::where('following_id', $user->id)
+                        ->select('followed_id')
+                        ->get()
+                        ->push($user->id)
+                 )->select('img_1', 'content')->get();
         return view('index', [
           'user' => $user,
           'posts' => $posts
