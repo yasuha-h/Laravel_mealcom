2cd1db70cc07993e392b4895b95ea6bc3aef9a4a
yasuha-h <kurukami135@gmail.com>
2020-03-11 22:07:01 +0900

    change_btn-layout

--- resources/views/common/navbar.blade.php
+++ resources/views/common/navbar.blade.php
@@ -1,16 +1,16 @@
 <nav class="nav col-1 text-center pr-0 border-right h-100 sticky">
   <ul class="list-unstyled w-100">
     <li>
-      <a href="/user/{{ $user->id }}" class="nav-link w-100 py-5 px-0">シェア</a>
+      <a href="/user/{{ $user->id }}" class="nav-link w-100 py-4 my-5 px-0 btn-info border rounded-circle">シェア</a>
     </li>
     <li>
-      <a href="/user/{{ $user->id }}/following" class="nav-link w-100 py-5 px-0">フォロー</a>
+      <a href="/user/{{ $user->id }}/following" class="nav-link w-100 py-4 my-5 px-0 btn-info border rounded-circle">フォロー</a>
     </li>
     <li>
-      <a href="/user/{{ $user->id }}/followers" class="nav-link w-100 py-5 px-0">フォロワー</a>
+      <a href="/user/{{ $user->id }}/followers" class="nav-link w-100 py-4 my-5 px-0 btn-info border rounded-circle">フォロワー</a>
     </li>
     <li>
-      <a href="/user/{{ $user->id }}/nices" class="nav-link w-100 py-5 px-0">いいね</a>
+      <a href="/user/{{ $user->id }}/nices" class="nav-link w-100 py-4 my-5 px-0 btn-info border rounded-circle">いいね</a>
     </li>
   </ul>
 </nav>
